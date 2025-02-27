b8eea39853cb9e11416d577a71cb865e89db36b0
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-02-27 17:18:30 +0100

    push3

--- library/SDL/build/CMakeFiles/SDL3-shared.dir/src/video/SDL_video_unsupported.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/SDL3-shared.dir/src/video/SDL_video_unsupported.c.o: \
- /home/daniel/Documents/Uni/Project/library/SDL/src/video/SDL_video_unsupported.c \
- /usr/include/stdc-predef.h \
- /home/daniel/Documents/Uni/Project/library/SDL/src/SDL_internal.h
--- library/SDL/build/CMakeFiles/SDL3-shared.dir/src/video/wayland/SDL_waylandmessagebox.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/SDL3-shared.dir/src/video/wayland/SDL_waylandmessagebox.c.o: \
- /home/daniel/Documents/Uni/Project/library/SDL/src/video/wayland/SDL_waylandmessagebox.c \
- /usr/include/stdc-predef.h \
- /home/daniel/Documents/Uni/Project/library/SDL/src/SDL_internal.h \
- /home/daniel/Documents/Uni/Project/library/SDL/src/video/wayland/SDL_waylandmessagebox.h
